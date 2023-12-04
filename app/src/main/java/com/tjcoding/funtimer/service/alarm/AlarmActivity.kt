60feac1ef017696f11b041201b15306c56a81fc9
timjeric1 <timjeric1@gmail.com>
2023-12-04 09:33:42 +0100

    added timerItem to intent

--- app/src/main/java/com/tjcoding/funtimer/service/alarm/AlarmActivity.kt
+++ app/src/main/java/com/tjcoding/funtimer/service/alarm/AlarmActivity.kt
@@ -2,17 +2,22 @@ package com.tjcoding.funtimer.service.alarm
 
 import android.app.KeyguardManager
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.tjcoding.funtimer.domain.model.TimerItem
-import com.tjcoding.funtimer.service.alarm.AlarmService.Companion.DISMISS_ALARM_ACTION
+import com.tjcoding.funtimer.service.alarm.AlarmService.Companion.DISMISS_AND_ADD_NOTIFICATION_ACTION
 import com.tjcoding.funtimer.service.alarm.presentation.AlarmScreen
 import com.tjcoding.funtimer.ui.theme.FunTimerTheme
 
 
 class AlarmActivity : ComponentActivity() {
+
+
+    private lateinit var timerItem: TimerItem
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,13 +30,16 @@ class AlarmActivity : ComponentActivity() {
                     WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
         )
 
-        val timerItem = intent?.getParcelableExtra("TIMER_ITEM", TimerItem::class.java) ?: return
+        timerItem = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent?.getParcelableExtra("TIMER_ITEM", TimerItem::class.java) ?: return
+        } else {
+            intent?.getParcelableExtra("TIMER_ITEM") ?: return
+        }
 
 
         setContent {
             FunTimerTheme {
                 AlarmScreen(numbers = timerItem.selectedNumbers, onDismiss = ::onDismiss
-
                 )
             }
         }
@@ -41,7 +49,8 @@ class AlarmActivity : ComponentActivity() {
 
     private fun onDismiss() {
         val dismissIntent = Intent(this, AlarmService::class.java)
-        dismissIntent.action = DISMISS_ALARM_ACTION
+        dismissIntent.action = DISMISS_AND_ADD_NOTIFICATION_ACTION
+        dismissIntent.putExtra("TIMER_ITEM", timerItem)
         startForegroundService(dismissIntent)
         finish()
     }
